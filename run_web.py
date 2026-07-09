3e0bca08dd3cb9ee5eeba4e4c61725f505dd141b
S Holland Staff 8924004 <SHolland@nuast.org>
2026-07-09 10:26:56 +0100

    Add history page and base template
    Refactors templates to use a base layout with shared navigation. Adds a /history route showing the last 50 conversions. Renames index.html to home.html.

--- run_web.py
+++ run_web.py
@@ -14,7 +14,13 @@ def index():
         db.save_input(binary)  # save.save_input(binary)
         answer = bin2hex(binary)
 
-    return render_template("index.html", answer=answer.upper(), recent_inputs=db.recent_input()) #save.recent_input()
+    return render_template("home.html", answer=answer.upper(), recent_inputs=db.recent_input()) #save.recent_input()
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html", recent_inputs=db.recent_input(50))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
